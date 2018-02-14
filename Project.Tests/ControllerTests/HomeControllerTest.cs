6560ad7f2140f843c7dc2df326ab5b0ac6d719e3
Frank Ngo <frankngomusic@gmail.com>
2018-02-14 09:04:33 -0800

    basic template

---
+++ Project.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Project.Controllers;
+using Project.Models;
+
+namespace Project.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+      
+    }
+}
